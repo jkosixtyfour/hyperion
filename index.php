490dbb1f09483d6449909bdb1e34bdf8f59d3187
RiskDevKit <risk61358@gmail.com>
2022-09-27 19:08:25 -0400

    the j

---
+++ index.php
@@ -0,0 +1,222 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd">
+
+
+
+
+<html>
+	
+	<title>hyperion - rebroadcast yourself</title>
+
+	<link rel="stylesheet" href="https://web.archive.org/web/20061214124217cs_/http://www.youtube.com/css/styles_yts1164775696.css" type="text/css">
+	<link rel="stylesheet" href="https://web.archive.org/web/20061214124217cs_/http://www.youtube.com/css/base_yts1166062673.css" type="text/css">
+	<link rel="icon" href="/web/20061214124217im_/http://www.youtube.com/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="/web/20061214124217im_/http://www.youtube.com/favicon.ico" type="image/x-icon">
+	
+	
+	<meta name="keywords" content="video,sharing,camera phone,video phone">
+
+	<link rel="alternate" title="YouTube - [RSS]" href="/web/20061214124217/http://www.youtube.com/rssls">
+
+	<script type="text/javascript" src="/web/20061214124217js_/http://www.youtube.com/js/ui_yts1164777409.js"></script>
+	<script type="text/javascript" src="/web/20061214124217js_/http://www.youtube.com/js/AJAX_yts1161839869.js"></script>
+	<script type="text/javascript" src="/web/20061214124217js_/http://www.youtube.com/js/watch_queue_yts1161839869.js"></script>
+	<script language="javascript" type="text/javascript">
+		onLoadFunctionList = new Array();
+		function performOnLoadFunctions()
+		{
+			for (var i in onLoadFunctionList)
+			{
+				onLoadFunctionList[i]();
+			}
+		}
+	</script>
+	
+	<script language="javascript" type="text/javascript"> function _hbLink (a,b) { return false; } </script>
+
+
+</head>
+
+
+<body onload="performOnLoadFunctions();">
+<div id="baseDiv">	
+		<div id="logoTagDiv">
+	<a href="." name="&amp;lid=Logo&amp;lpos=GlobalNav"><img src="https://web.archive.org/web/20061214124217im_/http://www.youtube.com/img/pic_youtubelogo_123x63.gif" alt="Home" width="123" height="63" border="0" onmouseover="showDiv('logoHomeTip');" onmouseout="hideDiv('logoHomeTip');"/></a>
+	</div>
+	<div id="logoHomeTip" style="display: none;">
+	Home
+	</div>
+
+	<div id="utilDiv">
+	
+		<b><a href="register.php">Sign Up</a></b>
+		<span class="utilDelim">|</span>
+		<a href="my_account.php">My Account</a>
+		<span class="utilDelim">|</span>
+		<a href="recently_watched.php">History</a>
+		<span class="utilDelim">|</span>
+		<!--<a href="/web/20061214124217/http://www.youtube.com/watch_queue?all" onclick="_hbLink('QuickList','UtilityLinks');">QuickList</a>
+		(<span id="quicklist_numb"><a href="/web/20061214124217/http://www.youtube.com/watch_queue?all"><script type="text/javascript">var quicklist_count=0;document.write(quicklist_count);</script></a></span>)                        
+        <span class="utilDelim">|</span>-->
+		<a href="help_center.php">Help</a>
+		<span class="utilDelim">|</span>                        
+		<a href="login.php">Log In</a>
+	<form name="logoutForm" method="post" action="logout.php">
+		<input type="hidden" name="action_logout" value="1">
+	</form>
+</div>
+
+	
+	<div id="searchDiv">
+		<form name="searchForm" id="searchForm" method="get" action="results.php">
+		<input tabindex="10000" type="text" name="search_query" maxlength="128" class="searchField" value="">
+		&nbsp;
+		<input type="submit" name="search" value="Search">
+	</form>
+
+	</div>
+		
+	<div id="gNavDiv">
+		
+		
+		<div id="upload"><a href="my_videos_upload.php"><img src="https://web.archive.org/web/20061214124217im_/http://www.youtube.com/img/pic_upload_130x28.gif" width="130" height="28" alt="upload" border="0"/></a></div>
+
+		<div class="tab">
+			<a href="community.php"><img src="https://web.archive.org/web/20061214124217im_/http://www.youtube.com/img/tab_community_118x28.gif" width="118" height="28" border="0" alt="community"/></a></div>
+		<div class="tab">
+			<a href="members.php"><img src="https://web.archive.org/web/20061214124217im_/http://www.youtube.com/img/tab_channels_118x28.gif" width="118" height="28" border="0" alt="channels"/></a></div>
+		<div class="tab">
+			<a href="categories.php"><img src="https://web.archive.org/web/20061214124217im_/http://www.youtube.com/img/tab_categories_118x28.gif" width="118" height="28" border="0" alt="categories"/></a></div>
+		<div class="tab">
+			<a href="browse.php"><img src="https://web.archive.org/web/20061214124217im_/http://www.youtube.com/img/tab_videos_118x28.gif" width="118" height="28" border="0" alt="videos"/></a></div>
+	</div>
+<!-- end gNavDiv -->
+		<div id="gNavBottom">&nbsp;</div>
+
+
+	
+	
+	
+	
+
+
+
+
+<script type="text/javascript" src="/web/20061214124217js_/http://www.youtube.com/js/components_yts1157352107.js"></script>
+<script type="text/javascript" src="/web/20061214124217js_/http://www.youtube.com/js/AJAX_yts1161839869.js"></script>
+<script type="text/javascript" src="/web/20061214124217js_/http://www.youtube.com/js/video_bar_yts1157352107.js"></script>
+
+<script type="text/javascript">
+</script>
+
+<h1>Welcome to the hyperion beta</h1>
+<p>Currently working on account system</p>
+<div>
+    <a href="login.php">Login</a>
+    <a href="register.php">Register</a>
+    <a href="account.php">Dashboard</a>
+    <hr>
+    <h2>Archives</h2>
+    <p>Archives used to build the site</p>
+    <h3>December 2006</h3>
+    <a href="https://web.archive.org/web/20061214124217/http://www.youtube.com/">Homepage 12/14/06</a>
+    <a href="https://web.archive.org/web/20061215011047/http://www.youtube.com/signup">Signup 12/14/06</a>
+        </div>
+
+		<div class="spacer">&nbsp;</div>
+	<div id="footerDiv">
+		<div id="footerContent">
+		<div id="footerSearch">
+			<form name="searchFormFooter" id="searchFormFooter" method="get" action="/web/20061214124217/http://www.youtube.com/results">
+			<input type="text" name="search_query" maxlength="128" class="searchField" value="">
+			&nbsp;
+			<input type="submit" name="search" value="Search">
+			</form>
+		</div> <!-- end footerSearch -->
+
+	
+		<div id="footerLinks">
+			
+			<table border="0" cellpadding="0" cellspacing="0" width="100%" align="center"><tr valign="top">
+			
+			<td>
+			<div class="footColumnLeft">
+				<div class="footLabel">Your&nbsp;&nbsp;Account</div>
+				<div class="footValues">
+					<div class="column">
+					<a href="/web/20061214124217/http://www.youtube.com/my_videos">Videos</a><br/>
+					<a href="/web/20061214124217/http://www.youtube.com/my_favorites">Favorites</a><br/>
+					</div>
+					<div class="column">
+					<a href="/web/20061214124217/http://www.youtube.com/my_playlists">Playlists</a><br/>
+					<a href="/web/20061214124217/http://www.youtube.com/my_messages">Inbox</a><br/>
+					</div>
+					<div class="column">
+					<a href="/web/20061214124217/http://www.youtube.com/subscription_center">Subscriptions</a><br/>
+					<a href="/web/20061214124217/http://www.youtube.com/my_account">more...</a><br/>
+					</div>
+				</div>
+			</div>
+			</td>
+			
+			<td>
+			<div class="footColumnMid">
+				<div class="footLabel">Help &amp; Info</div>
+				<div class="footValues">
+					<div class="column">
+					<a href="/web/20061214124217/http://www.youtube.com/t/help_center">Help Center</a><br/>
+					<a href="/web/20061214124217/http://www.youtube.com/t/video_toolbox">Video Toolbox</a><br/>
+					</div>
+					<div class="column">
+					<a href="/web/20061214124217/http://www.youtube.com/dev">Developer APIs</a><br/>
+					<a href="/web/20061214124217/http://www.youtube.com/t/safety">Safety Tips</a><br/>
+					</div>
+					<div class="column">
+					<a href="/web/20061214124217/http://www.youtube.com/t/dmca_policy">Copyright FAQ</a><br/>
+					<a href="/web/20061214124217/http://www.youtube.com/t/community_guidelines">Code of Conduct</a><br/>
+					</div>
+				</div>
+			</div>
+			</td>
+			
+			
+			<td>
+			<div class="footColumnRight">
+				<div class="footLabel">YouTube</div>
+				<div class="footValues">
+					<div class="column">
+					<a href="/web/20061214124217/http://www.youtube.com/t/about">Company Info</a><br/>
+					<a href="/web/20061214124217/http://www.youtube.com/testtube">Test Tube</a><br/>
+					</div>
+					<div class="column">
+					<a href="/web/20061214124217/http://www.youtube.com/t/terms">Terms of Use</a><br/>
+					<a href="/web/20061214124217/http://www.youtube.com/t/privacy">Privacy Policy</a><br/>
+					</div>
+					<div class="column">
+					<a href="/web/20061214124217/http://www.youtube.com/blog">Blog</a><br/>
+					<a href="/web/20061214124217/http://www.youtube.com/contact">Contact</a><br/>
+					</div>
+					<div class="column">
+					<a href="/web/20061214124217/http://www.youtube.com/press_room">Press</a><br/>
+					<a href="https://web.archive.org/web/20061214124217/http://www.pcrecruiter.net/pcrbin/regmenu.exe?uid=youtube.youtube">Jobs</a><br/>
+					</div>
+				</div>
+			</div>
+			</td>
+			
+			</tr></table>
+			
+		</div> <!-- end footerLinks -->
+		</div> <!-- end footerContent -->
+
+
+		<div id="footerCopyright">
+			Copyright &copy; 2022 hyperion, Inc.
+		</div> <!-- end footerCopyright -->
+		
+	</div> <!-- end footerDiv -->
+
+</div> <!-- end baseDiv -->
+</body>
+
+</html>

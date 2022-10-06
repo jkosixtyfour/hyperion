cbb540b25956b242519d5747026bd3c9d242ed7a
RiskDevKit <risk61358@gmail.com>
2022-10-05 22:54:08 -0400

    thats nuts bro

--- 2005/index.php
+++ 2005/index.php
@@ -941,7 +941,7 @@
 <table cellpadding="10" cellspacing="0" border="0" align="center">
 	<tr>
 		<td align="center" valign="center"><span class="footer"><a href="whats_new.php">What's New</a> | <a href="about.php">About Us</a> | <a href="help.php">Help</a> | <a href="developers_intro.php">Developers</a> <img src="/web/20050831230308im_/http://www.youtube.com/img/new.gif"> | <a href="terms.php">Terms of Use</a> | <a href="privacy.php">Privacy Policy</a> 
-		<br><br>Copyright &copy; 2005 YouTube, LLC&#8482; | <a href="rss/global/recently_added.rss"><img src="http://web.archive.org/web/20050831230308im_/http://www.youtube.com/img/rss.gif" width="36" height="14" border="0" style="vertical-align: text-top;"></a></span></td>
+		<br><br>Copyright &copy; 2022 hyperion, LLC&#8482; | <a href="rss/global/recently_added.rss"><img src="img/rss.gif" width="36" height="14" border="0" style="vertical-align: text-top;"></a></span></td>
 	</tr>
 </table>
 
